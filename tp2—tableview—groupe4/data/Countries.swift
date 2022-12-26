de0d572bc07b26c34fe8fe35b85dd7d42855077a
fatima-ezzahra-sahraoui <sahraoui.fatimazzahra@gmail.com>
2022-12-26 19:24:04 +0000

    Initial Commit

---
+++ tp2—tableview—groupe4/data/Countries.swift
@@ -0,0 +1,18 @@
+//
+//  Countries.swift
+//  tp2—tableview—groupe4
+//
+//  Created by user222272 on 12/25/22.
+//
+
+import Foundation
+let countries=[
+Country(isoCode: "at", name: "Austria"),
+Country(isoCode: "be", name: "Belgium"),
+Country(isoCode: "de", name: "Germany"),
+Country(isoCode: "el", name: "Greece"),
+Country(isoCode: "fr", name: "France"),
+Country(isoCode: "at", name: "Morocco"),
+Country(isoCode: "at", name: "Canada"),
+
+]
---
+++ tp2—tableview—groupe4/models/Country.swift
@@ -0,0 +1,11 @@
+//
+//  Country.swift
+//  tp2—tableview—groupe4
+//
+//  Created by user222272 on 12/25/22.
+//
+struct Country {
+    var isoCode:String
+    var name:String
+}
+import Foundation
